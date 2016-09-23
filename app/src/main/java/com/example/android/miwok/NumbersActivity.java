b7826cbf8f7ebc8c7e7e2adb03f9bbcd38b8a37b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-23 09:50:25 -0300

    Change Array for ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,35 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
+
+
+        // Create an array of words
+        String[] arrayWords = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+
+
+        ArrayList<String> words = new ArrayList<>();
+
+        for (String word : arrayWords) {
+            words.add(word);
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            Log.v(TAG, "Word at index " + i + ": " + words.get(i));
+        }
     }
 }
